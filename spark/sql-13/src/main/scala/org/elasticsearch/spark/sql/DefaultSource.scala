ccf86377af9298161e90ea2ae3447434eb36de12
Aniket Bhatnagar <abhatnagar4@sapient.com>
2015-04-30 20:36:39 +0530

    Initial draft for Spark SQL filter pushdown

--- spark/sql-13/src/main/scala/org/elasticsearch/spark/sql/DefaultSource.scala
+++ spark/sql-13/src/main/scala/org/elasticsearch/spark/sql/DefaultSource.scala
@@ -6,13 +6,11 @@ import scala.collection.mutable.LinkedHashMap
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.sources.BaseRelation
-import org.apache.spark.sql.sources.PrunedScan
-import org.apache.spark.sql.sources.RelationProvider
 import org.apache.spark.sql.types.StructType
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 import org.elasticsearch.hadoop.util.StringUtils
 import org.elasticsearch.spark.cfg.SparkSettingsManager
+import org.apache.spark.sql.sources._
 
 private[sql] class DefaultSource extends RelationProvider {
   override def createRelation(
@@ -30,7 +28,7 @@ private[sql] class DefaultSource extends RelationProvider {
 }
 
 private[sql] case class ElasticsearchRelation(parameters: Map[String, String])(@transient val sqlContext: SQLContext)
-  extends BaseRelation with PrunedScan //with InsertableRelation
+  extends BaseRelation with PrunedFilteredScan //with InsertableRelation
   {
 
   @transient lazy val cfg = {
@@ -52,8 +50,56 @@ private[sql] case class ElasticsearchRelation(parameters: Map[String, String])(@
     paramWithProjection += (ConfigurationOptions.ES_SCROLL_FIELDS -> StringUtils.concatenate(requiredColumns.asInstanceOf[Array[Object]], ","))
     new ScalaEsRowRDD(sqlContext.sparkContext, paramWithProjection)
   }
+  
+  // PrunedFilteredScan
+  def buildScan(requiredColumns: Array[String], filters: Array[Filter]) = {
+    val paramWithFilter = LinkedHashMap[String, String]() ++ parameters
+    paramWithFilter += (ConfigurationOptions.ES_SCROLL_FIELDS -> StringUtils.concatenate(requiredColumns.asInstanceOf[Array[Object]], ","))
+    
+    val esQueries = filters.map(filter => buildQuery(filter)).filter(query => query.trim().length() > 0)
+    val esQuery = esQueries.mkString("""
+      {
+        "query": {
+          "filtered": {
+            "filter": {
+              "and": {
+                "filters": [
+      """, 
+      ",", 
+      """
+                ]
+              }
+            }
+          }
+        }
+      }""")
+    paramWithFilter += (ConfigurationOptions.ES_QUERY -> esQuery)
+   
+    new ScalaEsRowRDD(sqlContext.sparkContext, paramWithFilter)
+  }
 
   def insert(data: DataFrame, overwrite: Boolean) {
     throw new UnsupportedOperationException()
+  }
+  
+  private def buildQuery(filter: Filter): String = {
+    filter match {
+      case EqualTo(attribute, value)            => s"""{ "term" : { "$attribute" : "$value"} }"""
+      case GreaterThan(attribute, value)        => s"""{ "range" : { "$attribute": { "gt" : "$value" } } }"""
+      case GreaterThanOrEqual(attribute, value) => s"""{ "range" : { "$attribute": { "gte" : "$value" } } }"""
+      case LessThan(attribute, value)           => s"""{ "range" : { "$attribute": { "lt" : "$value" } } }"""
+      case LessThanOrEqual(attribute, value)    => s"""{ "range" : { "$attribute": { "lte" : "$value" } } }"""
+      case In(attribute, values)                => val valuesSerialized = values.mkString("[\"", "\",\"", "\"]")
+                                                   s"""{ "terms" : { "$attribute" : ["$valuesSerialized]} }"""
+      case IsNull(attribute)                    => s"""{ "missing" : { "field" : "$attribute" } }"""
+      case IsNotNull(attribute)                 => s"""{ "exists" : { "field" : "$attribute" } }"""
+      case And(left, right)                     => s"""{ "and" : { "filters" : [ ${buildQuery(left)}, ${buildQuery(right)} ] } }"""
+      case Or(left, right)                      => s"""{ "or" : { "filters" : [ ${buildQuery(left)}, ${buildQuery(right)} ] } }"""
+      case Not(filterToNeg)                     => s"""{ "not" : { "filter" : ${buildQuery(filterToNeg)} } }"""
+      case StringStartsWith(attribute, value)   => s"""{ "regexp": { "attribute" : "$value.*" } }"""
+      case StringEndsWith(attribute, value)     => s"""{ "regexp": { "attribute" : ".*$value" } }"""
+      case StringContains(attribute, value)     => s"""{ "regexp": { "attribute" : ".*$value.*" } }"""
+      case _ => ""
+    }
   }
 }
